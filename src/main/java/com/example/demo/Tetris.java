d3a50748566c165ef7a3570963b1d7a85f0e8de5
Candela Cano Gimenez <candecano11@gmail.com>
2024-09-01 20:14:22 -0300

    Add: Candela, se restaura los cambios de carla + creacion de piezas perro izq,der,stick y t

--- src/main/java/com/example/demo/Tetris.java
+++ src/main/java/com/example/demo/Tetris.java
@@ -1,20 +1,19 @@
 package com.example.demo;
 
 public class Tetris {
-    public int [][] piezas;
+    public int[][] piezas;
     public Tetris(){
     }
-    
-    public Tetris (int [][] piezas){
+   
+    public Tetris(int[][] piezas) {
         this.piezas = piezas;
     }
-    public int getPiezas(int fila, int columna){
-       return piezas[fila][columna];
+  
+    public int getPiezas(int fila, int columna) {
+        return piezas[fila][columna];
     }
-    public void setPiezas(int fila, int columna, int valor){
+    public void setPiezas(int fila, int columna, int valor) {
         piezas[fila][columna] = valor;
-
     }
 
- 
 }
