425db94187878ac4f21d2af8b1a73f694c2af891
Carla Marturet <abrevadero.4020@gmial.com>
2024-09-03 18:15:04 -0300

    se rotan las piezas perro izquierda derecha, más test
    Co-authored-by: Candecano <Candecano@users.noreply.github.com>

--- src/test/java/com/example/demo/PieceDogLeftTest.java
+++ src/test/java/com/example/demo/PieceDogLeftTest.java
@@ -1,6 +1,5 @@
 package com.example.demo;
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -20,6 +19,53 @@ public class PieceDogLeftTest {
         assertEquals(1, pidoglefT.getPiezas(0, 1));
         assertEquals(1, pidoglefT.getPiezas(1, 1));
         assertEquals(1, pidoglefT.getPiezas(1, 2));
-            
-                }
+        
+  }
+  @Test
+  void rotar_dogleft_a_izq_(){
+    int[][] piezas = {
+      {1,1,0},
+      {0,1,1}
+      
+ };
+      PieceDogLeft pidoglefT = new PieceDogLeft(piezas);
+      
+      assertEquals(1, pidoglefT.getPiezas(0, 0));
+      assertEquals(1, pidoglefT.getPiezas(0, 1));
+      assertEquals(1, pidoglefT.getPiezas(1, 1));
+      assertEquals(1, pidoglefT.getPiezas(1, 2));
+
+      //se rota la pieza perro izq a la izq
+
+     pidoglefT.rotarizq();
+      assertEquals(1, pidoglefT.getPiezas(0, 1));
+      assertEquals(0, pidoglefT.getPiezas(0, 0));
+      assertEquals(1, pidoglefT.getPiezas(1, 0));
+      assertEquals(1, pidoglefT.getPiezas(1, 1));
+      assertEquals(1, pidoglefT.getPiezas(2, 0));
+      assertEquals(0, pidoglefT.getPiezas(2, 1));
+      
+//se vuelve a rotar la pieza a la izq y vuelve a su forma original
+      pidoglefT.rotarizq();
+      assertEquals(1, pidoglefT.getPiezas(0, 0));
+      assertEquals(1, pidoglefT.getPiezas(0, 1));
+      assertEquals(1, pidoglefT.getPiezas(1, 1));
+      assertEquals(1, pidoglefT.getPiezas(1, 2));
+
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+                
 }
